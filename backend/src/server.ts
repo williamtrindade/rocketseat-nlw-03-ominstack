43a390ca24d94373a0d1e268a3cbfa2b0aac9e07
William Trindade <williamtrindade777@gmail.com>
2020-10-13 13:54:21 -0300

    feat: Configuting type orm

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import './database/connection';
 
 const app = express();
 app.use(express.json());
 
 app.get('/api/users', (request, response) => {
-    return response.json({ messages: 'OLA' });
+  return response.json({ messages: 'OLA' });
 });
 
 app.listen(3333);
