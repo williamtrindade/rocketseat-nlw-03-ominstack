3996a4f6c29ce05140783bb775a6bb6cb18c0455
William Trindade <williamtrindade777@gmail.com>
2020-10-13 13:37:26 -0300

    feat: Enable Node to receive json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 
 const app = express();
-app.get('/', (request, response) => {
-    return response.json({'messages': 'OLA'});
+app.use(express.json());
+
+app.get('/api/users', (request, response) => {
+    return response.json({ messages: 'OLA' });
 });
+
 app.listen(3333);
