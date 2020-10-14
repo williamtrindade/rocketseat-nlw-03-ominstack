596dfb261171c571f5416bcb1cbc6b49a291f94a
William Trindade <williamtrindade777@gmail.com>
2020-10-14 12:02:32 -0300

    Setting routes to pass files to controller

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 import OrphanagesController from './controllers/OrphanagesController';
+import uploadConfig from './config/upload';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
-routes.post('/api/orphanages', OrphanagesController.create)
+routes.post('/api/orphanages', upload.array('images'), OrphanagesController.create)
 routes.get('/api/orphanages', OrphanagesController.index)
 routes.get('/api/orphanages/:id', OrphanagesController.read)
 
