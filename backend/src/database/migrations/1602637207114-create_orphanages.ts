483cd1a6488f15cab51cf44d7252e0acbbda0070
William Trindade <williamtrindade777@gmail.com>
2020-10-14 20:28:06 -0300

    Using views(Transformers)

--- backend/src/database/migrations/1602637207114-create_orphanages.ts
+++ backend/src/database/migrations/1602637207114-create_orphanages.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class createOrphanages1602637207114 implements MigrationInterface {
 
