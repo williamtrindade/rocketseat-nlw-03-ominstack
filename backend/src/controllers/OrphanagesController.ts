9593e06448c296a56e5567fd229591305549e0bb
William Trindade <williamtrindade777@gmail.com>
2020-10-14 13:14:50 -0300

    Saving images relation in cascade

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -30,6 +30,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -37,7 +42,8 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends
+      open_on_weekends,
+      images
     });
 
     await orphanagesRepository.save(orphanage);
