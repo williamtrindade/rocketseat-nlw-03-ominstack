b8683ad00da5c67bc2717c3df3ce2427e7a387db
William Trindade <williamtrindade777@gmail.com>
2020-10-14 21:09:54 -0300

    Validating with Yup and adding cors

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -39,7 +39,7 @@ export default {
     const requestImages = request.files as Express.Multer.File[];
     const images = requestImages.map(image => {
       return { path: image.filename }
-    })
+    });
 
     const data = {
       name,
@@ -51,6 +51,23 @@ export default {
       open_on_weekends,
       images
     };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(Yup.object().shape({
+        path: Yup.string().required()
+      })),
+    });
+    console.log(data.open_on_weekends);
+    await schema.validate(data, {
+      abortEarly: false,
+    });
 
     const orphanage = orphanagesRepository.create(data);
 
