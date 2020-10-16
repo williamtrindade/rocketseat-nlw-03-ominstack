66a09df068f77d0d74639dbbc16b9f956e1a3e4a
William Trindade <williamtrindade777@gmail.com>
2020-10-15 23:29:44 -0300

    feat: (Mobile) Finalizing navigation and map

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,45 +1,26 @@
 import React from 'react';
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-
-import mapMarker from './src/images/map-marker.png';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+import Router from './src/routes';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold, 
+    Nunito_700Bold, 
+    Nunito_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map} 
-        initialRegion={{
-          latitude: -29.6918979,
-          longitude: -53.856333,
-          latitudeDelta: 0.000,
-          longitudeDelta: 0.000,
-        }} 
-        >
-          <Marker 
-            icon={mapMarker}
-            coordinate={{
-              latitude: -29.6918979,
-              longitude: -53.856333,
-            }}
-          >
-          </Marker>
-        </MapView>
-    </View>
+   <Router></Router>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-  },
-});
+
 
 //-29.6918979,-53.856333,15z
